b1dd7c8250cfd697232cdc362a157a45241a04ff
Donald Whitely <dswhitely1@gmail.com>
2019-11-28 09:21:28 -0500

    Suppressed TS Warnings in test

--- src/app/shared/dropdown.directive.spec.ts
+++ src/app/shared/dropdown.directive.spec.ts
@@ -2,6 +2,7 @@ import { DropdownDirective } from './dropdown.directive';
 
 describe('DropdownDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new DropdownDirective();
     expect(directive).toBeTruthy();
   });
